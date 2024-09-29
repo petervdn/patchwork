0b73361e6b49190c07e59b92fa8cbcb5ea4a1cb1
Peter van der Noord <peterdunord@gmail.com>
2024-09-29 21:33:45 +0200

    Add empty xcode project

---
+++ engine/patchwork/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  patchwork
+//
+//  Created by Peter van der Noord on 29/09/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
